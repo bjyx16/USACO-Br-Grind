bb8f41c2cb81f5627cf8b0fd739c0903f58a10dc
bjyx16 <136404486+bjyx16@users.noreply.github.com>
2023-07-16 11:55:02 -0400

    Create milk_factory.cpp

---
+++ Prev. Bronze Problems/milk_factory.cpp
@@ -0,0 +1,71 @@
+/*
+- O(n^3)
+- DFS: for each possible special station m, test if can get to m from station i
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+int n;
+int m;
+bool visit[101]={};
+vector< vector<int> > edges(10000+10);
+
+void dfs(int curr){
+	visit[curr]=1;
+	for (vector<int>::iterator it=edges[curr].begin();it!=edges[curr].end();it++){
+		int x=*it;
+		//cout<<x<<" ";
+		dfs(*it);
+	}
+}
+
+int main(){
+
+	freopen("factory.in","r",stdin);
+	freopen("factory.out","w",stdout);
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	bool ans=true;
+	cin>>n;
+	//edges = set<int> [n];
+	edges = vector< vector<int> > (n);//why doesn;t vector<int> edges[n] work
+
+	for (int i=0;i<n-1;i++){
+		int a,b;
+		cin>>a>>b;
+		edges[--a].push_back(--b);//stored from [1,N]
+	}
+
+	for (m=0;m<n;m++){
+		bool ans=true;
+		for (int i=0;i<n;i++){//test each node
+		    if (m==i){
+		         continue;
+		    }
+		    for (int i=0;i<n;i++){
+			    visit[i]=0;
+		    }
+			dfs(i);
+    		if (visit[m]==0){//if you cant get from j to i
+    		    ans=false;
+    		    break;
+		   }
+        }
+		
+		if (ans){
+			cout<<m+1;
+			return 0;
+		}
+	}
+
+	cout<<-1;
+
+	return 0;
+}
