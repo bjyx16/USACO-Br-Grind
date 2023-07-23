08f53acdc04badca874195438f7a0ca42fbd6417
bjyx16 <136404486+bjyx16@users.noreply.github.com>
2023-07-23 14:36:15 -0400

    Update e1.cpp

--- cf_883/e1.cpp
+++ cf_883/e1.cpp
@@ -2,28 +2,36 @@
 #include <set>
 #include <cmath>
 using namespace std;
-
+ 
 int main(){
 	int t,n;
 	cin>>t;
-
+ 
 	set<int> s;
-	s.insert(1);
-	for (int i=1;i<=10;i++){
-		int temp=1;
-		while (pow(i,temp)<=1e6){
-			s.insert(pow(i,temp));
+	//s.insert(1);
+	for (int i=2;i<=1000;i++){
+		int temp=2;
+		int y=pow(i,temp)+i+1;
+		long long prev=i+1;
+		while (y<=1e6){
+			s.insert(y);
+			prev = y;
 			temp++;
+			y = pow(i,temp) + prev;
 		}
 	}
-
+ 
 	for (int i=0;i<t;i++){
 		cin>>n;
 		if (n<=3){
-			cout<<"NO";
+			cout<<"NO"<<endl;
 			continue;
-		}else if (s.count(n)==1){
-			cout<<"YES";
+		}else{
+		    if (s.count(n)==1){
+			    cout<<"YES";
+		    }else{
+		        cout<<"NO";
+		    }
 		}
 		cout<<endl;
 	}
