0a9ca8d15aca29c31ce09ec0045177fadf6f2499
bjyx16 <136404486+bjyx16@users.noreply.github.com>
2024-03-07 04:52:36 -0500

    Update cow_frisbee.cpp

--- silver_problems/cow_frisbee.cpp
+++ silver_problems/cow_frisbee.cpp
@@ -6,25 +6,29 @@
 #include<algorithm>
 using namespace std;
 
-int N;
+int n;
 long long ans=0;
 
-void add_contribution(const vector<int>& h) {
-	stack<int> stk;
-	for (int i = N-1; i >= 0; --i) {
-		while (!stk.empty() && h[stk.top()] < h[i]) stk.pop();
-		if (!stk.empty()) ans += stk.top()-i+1;
-		stk.push(i);
+void add_contribution(vector<int>& h) {
+	stack<int> s;
+	for (int i = n-1; i >= 0; i--) {
+		while (!s.empty() && h[s.top()] < h[i]){ s.pop(); }
+		if (!s.empty()){
+			ans += s.top()-i+1;
+		}
+		s.push(i);
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> N;
-	vector<int> h(N); for (int& i: h) cin >> i;
+	cin >> n;
+	vector<int> h(n); 
+	for (int i=0;i<n;i++){cin >> h[i];}
+	
 	add_contribution(h);
-	reverse(begin(h), end(h));
+	reverse(h.begin(), h.end());
 	add_contribution(h);
 	cout << ans << "\n";
 }
